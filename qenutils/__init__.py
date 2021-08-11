56e814ef74886a6aee61385d3db1ea45b07bb875
qenu <xchedeiklo@gmail.com>
2021-08-11 22:04:58 +0800

    update

--- qenutils/__init__.py
+++ qenutils/__init__.py
@@ -2,12 +2,7 @@ import json
 from pathlib import Path
 
 from redbot.core.bot import Red
-
 from .qenutils import Qenutils
-
-with open(Path(__file__).parent / "info.json") as fp:
-    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
-
 
 async def setup(bot: Red) -> None:
     bot.add_cog(Qenutils(bot))
