9e0b6315072501dd3e20373aeeca432b8b9efbc8
qenu <xchedeiklo@gmail.com>
2021-08-21 14:48:51 +0800

    ver1.0

--- twbnschat/twbnschat.py
+++ twbnschat/twbnschat.py
@@ -6,8 +6,6 @@ import asyncio
 import hashlib
 
 from selenium import webdriver
-from chromedriver_py import binary_path
-
 
 import discord
 from redbot.core import commands
@@ -30,7 +28,7 @@ class twBNSchat(commands.Cog):
         self.bot = bot
         self._sync = False
         self._enabled = True
-        # self.bot.loop.create_task(self.initialize())
+        self._cached_messages = list()
 
         self.config = Config.get_conf(
             self,
@@ -43,17 +41,16 @@ class twBNSchat(commands.Cog):
             "toggle": False,
         }
 
-        default_global = {
-            # "accountA": None,
-            # "accountB": None,
-            # "timestamp" : None,
-            "url": "",
-        }
+        # default_global = {
+        #     "accountA": None,
+        #     "accountB": None,
+        #     "timestamp" : None,
+        #     "url": "",
+        # }
 
         self.config.register_guild(**default_guild)
-        self.config.register_global(**default_global)
+        # self.config.register_global(**default_global)
 
-        # self.bot.loop.create_task(self.initialize())
         self.initialize()
 
     async def red_delete_data_for_user(
@@ -92,7 +89,9 @@ class twBNSchat(commands.Cog):
             desired_capabilities=driver_caps,
             executable_path=r"/usr/bin/chromedriver",
         )
-        self.driver.get('https://a90ur5.github.io/twBNS_F8ChattingChannel/web/index.html')
+        self.driver.get(
+            "https://a90ur5.github.io/twBNS_F8ChattingChannel/web/index.html"
+        )
         self._sync = self.bot.loop.create_task(self.start_fetch())
 
     async def start_fetch(self):
@@ -123,20 +122,38 @@ class twBNSchat(commands.Cog):
         if not len(guild_queue):
             return
 
-        embed = discord.Embed(title=data["player"], description=data["msg"], color = self.string2discordColor(data["player"]))
+        if self.in_cached(data["player"] + "|" + data["msg"]):
+            return
+
+        embed = discord.Embed(
+            title=data["player"],
+            description=data["msg"],
+            color=self.string2discordColor(data["player"]),
+        )
         embed.set_footer(text=data["time"])
         for guild_id in guild_queue:
             guild = self.bot.get_guild(guild_id)
             channel = guild.get_channel(int(config[guild_id]["channel"]))
             await channel.send(embed=embed)
 
-    def string2discordColor(self, text:str) -> str:
-        hashed = str(int(hashlib.sha1(text.encode("utf-8")).hexdigest(), 16) % (10 ** 9))
+    def string2discordColor(self, text: str) -> str:
+        hashed = str(
+            int(hashlib.sha1(text.encode("utf-8")).hexdigest(), 16) % (10 ** 9)
+        )
         r = int(hashed[:3]) % 255
         g = int(hashed[3:6]) % 255
         b = int(hashed[6:]) % 255
 
         return discord.Color.from_rgb(r, g, b)
+
+    def in_cached(self, text: str) -> bool:
+        if text in self._cached_messages:
+            return True
+        else:
+            self._cached_messages.append(text)
+            while len(self._cached_messages) >= 30:
+                self._cached_messages.pop(0)
+            return False
 
     @commands.group(name="twbnschat")
     @commands.admin_or_permissions(manage_guild=True)
@@ -152,7 +169,11 @@ class twBNSchat(commands.Cog):
             )
             embed.add_field(name="Enabled", value=config["toggle"])
 
-            channel = ctx.guild.get_channel(config["channel"]).mention if ctx.guild.get_channel(config["channel"]) else "None"
+            channel = (
+                ctx.guild.get_channel(config["channel"]).mention
+                if ctx.guild.get_channel(config["channel"])
+                else "None"
+            )
 
             embed.add_field(
                 name="Channel",
@@ -197,7 +218,8 @@ class twBNSchat(commands.Cog):
 
         await ctx.send(f"twbnschat has been {'enabled' if boo else f'disabled'}.")
 
-    async def test_send(self, item):
-        g = self.bot.get_guild(247820107760402434)
-        c = g.get_channel(879630016856596521)
-        await c.send(content=str(item))
+    @twbnschat.command(name="checkcache")
+    @commands.is_owner()
+    async def checkcache(self, ctx: commands.Context):
+        """(debug) function used to view cached messages"""
+        await ctx.send(content=self.in_cached)
