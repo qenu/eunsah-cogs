3868271c1508601cc38b00971522b2b4bcee648a
qenu <xchedeiklo@gmail.com>
2021-08-27 17:02:14 +0800

    update

--- twbnschat/twbnschat.py
+++ twbnschat/twbnschat.py
@@ -116,7 +116,7 @@ class twBNSchat(commands.Cog):
             wsJson = json.loads(wsData["message"])
             if (
                 wsJson["message"]["method"] == "Network.webSocketFrameReceived"
-                and wsJson["message"]["params"]["response"]["payloadData"][:2] == "42"
+                and wsJson["message"]["params"]["response"]["payloadData"][0] == "4"
             ):
                 await self.test_send(f"start processing")
 
